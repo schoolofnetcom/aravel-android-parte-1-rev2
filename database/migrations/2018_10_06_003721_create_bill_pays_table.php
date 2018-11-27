0657fed3812176537194affe5fac1c003f4b5a5c
Luiz <argentinaluiz@gmail.com>
2018-11-27 08:18:12 -0200

    Add unsigned method in migrations

--- database/migrations/2018_10_06_003721_create_bill_pays_table.php
+++ database/migrations/2018_10_06_003721_create_bill_pays_table.php
@@ -19,9 +19,9 @@ class CreateBillPaysTable extends Migration
             $table->date('date_due');
             $table->decimal('value');
             $table->boolean('done')->default(0);
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('category_id');
+            $table->integer('category_id')->unsigned();
             $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
